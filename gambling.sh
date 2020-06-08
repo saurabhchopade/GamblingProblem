9292b168a4862c3193062ff99398849d643c2ff2
saurabh chopade <saurabhchopade3250@gmail.com>
2020-06-08 16:36:19 +0530

    Added after 20 days Win Loose Amount

--- gambling.sh
+++ gambling.sh
@@ -1,33 +1,56 @@
 #!/bin/bash -x
-
-stake=100;
+declare -a totalWonAmount;
+declare -a totalLooseAmount;
+WINLOOSEAMOUNT=50;
 BET=1;
 MINSTAKE=50;
 MAXSTAKE=150;
-#while Loop Run Until Gambler win or loose 50 percent
-	while :
-	do
-		result=$((RANDOM%2));
-	 	if [ $result -eq 1 ]
-		then
-			echo "Won";
-			stake=$(($stake+$BET));
-				if [ $stake -eq $MAXSTAKE ]
+#This is  we calculating for only One Month
+		for (( day=1;day<31;day++ ))
+		do
+			stake=100;
+			flag=0;
+			while :
+				do
+				result=$((RANDOM%2));
+	 			if [ $result -eq 1 ]
 				then
-					echo "=Resigned=";
-					echo "Stake At the End Of Day="$stake;
-					exit 1;
-				fi;
-		else
-			echo "Loose";
-			stake=$(($stake-$BET));
-				if [ $stake -eq $MINSTAKE ]
-				then
-					echo "=Resigned=";
-					echo "Stake At the End of Day="$stake;
-					exit 1;
-				fi;
-		fi
+					echo "Won";
+					stake=$(($stake+$BET));
+					if [ $stake -eq $MAXSTAKE ]
+					then
+						echo "=Resigned=";
+						echo "Stake At the End Of Day="$stake;
+						flag=1;
+						break;
+					fi;
+				else
+					echo "Loose";
+					stake=$(($stake-$BET));
+					if [ $stake -eq $MINSTAKE ]
+					then
+						echo "=Resigned=";
+						echo "Stake At the End of Day="$stake;
+						flag=2;
+						break;
+					fi;
+				fi
 
-	echo "Final stake="$stake;
-	done
+			done
+			
+#This is for storing the winning amount of each day in a array
+			if [ $flag -eq 1 ]
+			then
+				totalWonAmount[((day))]=$WINLOOSEAMOUNT;
+			else
+				totalLooseAmount[((day))]=$WINLOOSEAMOUNT;
+			fi;
+
+#Here We Displaying Total amount Win Loose each day after date 20
+			if [ $day -ge 20 ]
+			then
+			echo "TOTAL WON AMOUNT="$(($(printf '%s +' "${totalWonAmount[@]}" )0));
+			echo "TOTAL LOOSE AMOUNT="$(($(printf '%s +' "${totalLooseAmount[@]}" )0));
+			fi;
+							
+		done
