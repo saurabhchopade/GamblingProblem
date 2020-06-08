cee1feedb73d0fb18c9cf8a3664bb8c8e298d243
saurabh chopade <saurabhchopade3250@gmail.com>
2020-06-08 13:33:52 +0530

    Added win loose condition

--- gambling.sh
+++ gambling.sh
@@ -1,5 +1,19 @@
 #!/bin/bash -x
 
-STAKE=100;
+stake=100;
 BET=1;
+#while Loop Run Until Gambler Loose All stake Upto $1
+	while [ $BET -lt $stake ]
+	do
+		result=$((RANDOM%2));
+	 	if [ $result -eq 1 ]
+		then
+			echo "Win";
+			stake=$(($stake+$BET));
 
+		else
+			echo "Loose";
+			stake=$(($stake-$BET));
+		fi
+	echo "Final stake"$stake;
+	done
