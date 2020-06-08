7b3825e3c42f2fa17c753231bd3e8b51385c75b2
saurabh chopade <saurabhchopade3250@gmail.com>
2020-06-08 14:00:27 +0530

    Added 50 percent win loose case

--- gambling.sh
+++ gambling.sh
@@ -2,18 +2,32 @@
 
 stake=100;
 BET=1;
-#while Loop Run Until Gambler Loose All stake Upto $1
-	while [ $BET -lt $stake ]
+MINSTAKE=50;
+MAXSTAKE=150;
+#while Loop Run Until Gambler win or loose 50 percent
+	while :
 	do
 		result=$((RANDOM%2));
 	 	if [ $result -eq 1 ]
 		then
-			echo "Win";
+			echo "Won";
 			stake=$(($stake+$BET));
-
+				if [ $stake -eq $MAXSTAKE ]
+				then
+					echo "=Resigned=";
+					echo "Stake At the End Of Day="$stake;
+					exit 1;
+				fi;
 		else
 			echo "Loose";
 			stake=$(($stake-$BET));
+				if [ $stake -eq $MINSTAKE ]
+				then
+					echo "=Resigned=";
+					echo "Stake At the End of Day="$stake;
+					exit 1;
+				fi;
 		fi
-	echo "Final stake"$stake;
+
+	echo "Final stake="$stake;
 	done
